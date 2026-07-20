9a52d33de527ea1982208dff1a87bc2bdce9398d
Yatheswar Ethalapaka <yatheswar546@gmail.com>
2026-07-21 01:56:58 +0530

    db/user-schema-created

---
+++ models/User.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User =
+  models.User || model("User", UserSchema);
