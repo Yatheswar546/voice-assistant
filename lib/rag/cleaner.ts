3f010ef50fa1b1db5f8059c3023b9ae14f0a43b8
Yatheswar Ethalapaka <yatheswar546@gmail.com>
2026-09-17 06:15:19 +0530

    cleaner-is-added

---
+++ lib/rag/cleaner.ts
@@ -0,0 +1,8 @@
+export function cleanText(text: string): string {
+  return text
+    .replace(/\r\n/g, "\n")
+    .replace(/\r/g, "\n")
+    .replace(/[ \t]+/g, " ")
+    .replace(/\n{3,}/g, "\n\n")
+    .trim();
+}
